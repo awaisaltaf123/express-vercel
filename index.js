b47d745aeb40017ffa3235d1da75fb40bb485e0d
Awais Altaf <awais.altaf@onepeloton.com>
2023-06-12 23:00:21 -0700

    added cors

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const product = require("./api/product");
 
 app.use(express.json({ extended: false }));
 
-app.use("/api/testprecor", product);
+// Enable CORS
+app.use(cors({
+  origin: ['http://express-vercel-tau-three.vercel.app']
+}));
+
+app.use("/api/testprecor", precor);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
