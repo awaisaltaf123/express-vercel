e5832f85015dad13bb016c69cd4ab2fe606192ed
awaisaltaf123 <98595364+awaisaltaf123@users.noreply.github.com>
2023-06-13 23:37:16 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,13 +5,6 @@ const product = require("./api/product");
 
 app.use(express.json({ extended: false }));
 
-// Configure CORS
-const corsOptions = {
-  origin: ["null", "https://express-vercel-tau-three.vercel.app"],
-  methods: ["GET", "POST"],
-};
-
-app.use(cors(corsOptions));
 
 app.use("/api/testprecor", product);
 
